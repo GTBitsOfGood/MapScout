86458f1f96877ede494e0f2e3f27ffc988a146ea
Jakob Johnson <johnsonjakob99@gmail.com>
2019-09-17 20:02:43 -0400

    Fixed react-bootstrap importing issues

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import * as RB from 'react-bootstrap';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Button from "react-bootstrap/Button";
 import CsvUpload from './CsvUpload';
 import NavBar from './NavBar';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actions from '../actions';
+
 
 const mapStateToProps = state => (state.mainReducer);
 
@@ -20,26 +24,26 @@ class About extends Component {
         return (
             <div>
                 <NavBar/>
-                <RB.Grid>
+                <Container>
                     {/*TODO: Make login work*/}
-                    <RB.Jumbotron>
-                        <RB.Form>
-                            <RB.FormGroup controlId="formBasicEmail">
-                                <RB.Label>Email address</RB.Label>
-                                <RB.FormControl type="email" placeholder="Enter email" />
-                            </RB.FormGroup>
-                            <RB.FormGroup controlId="formBasicPassword">
-                                <RB.Label>Password</RB.Label>
-                                <RB.FormControl type="password" placeholder="Password" />
-                            </RB.FormGroup>
-                            <RB.Button variant="primary" type="submit">
+                    <Jumbotron>
+                        <Form>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
                                 Submit
-                            </RB.Button>
-                        </RB.Form>
-                    </RB.Jumbotron>
+                            </Button>
+                        </Form>
+                    </Jumbotron>
                     {/*TODO: Hide CSV if user hasn't logged in*/}
                     <CsvUpload uploadCsv={file => this.props.uploadCsv(file)} />
-                </RB.Grid>
+                </Container>
             </div>
         )
     }
