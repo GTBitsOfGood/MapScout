9d738f61705faaff9606a1219996322bf9dcaedd
Jakob Johnson <johnsonjakob99@gmail.com>
2019-09-23 21:15:53 -0400

    Added react-localization dependency and added boilerplate

---
+++ src/utils/Localization.jsx
@@ -0,0 +1,14 @@
+import LocalizedStrings from 'react-localization';
+
+let strings = new LocalizedStrings({
+    en: {
+        email: "Email Address",
+        password: "Password",
+    },
+    es: {
+        email: "Dirección de correo electrónico",
+        password: "Contraseña",
+    }
+});
+
+export default strings;
