77964a1e8a48e39cfd62394f24cb1adea880cfd3
Jakob Johnson <johnsonjakob99@gmail.com>
2019-09-28 17:14:01 -0400

    Login page localization complete

--- src/utils/Localization.jsx
+++ src/utils/Localization.jsx
@@ -1,14 +1,35 @@
 import LocalizedStrings from 'react-localization';
 
-let strings = new LocalizedStrings({
+let localizationStrings = new LocalizedStrings({
     en: {
-        email: "Email Address",
-        password: "Password",
+        //auth page text
+        header: "Provider Login",
+        description: "Please verify that you are with the PACTS network",
+        emailLabel: "Email Address",
+        emailPlaceholder: "Enter Email",
+        passwordLabel: "Password",
+        passwordPlaceholder: "Password",
+        submit: "Submit",
+        signUp: "Sign Up",
+        //nav bar text
+        home: "Home ",
+        facilityUpload: "Facility Upload",
+
     },
     es: {
-        email: "Dirección de correo electrónico",
-        password: "Contraseña",
+        //auth page text
+        header: "Inicio de sesión del proveedor",
+        description: "Verifica que estás con la red PACTS",
+        emailLabel: "Dirección de correo electrónico",
+        emailPlaceholder: "Ingrese correo electrónico",
+        passwordLabel: "Contraseña",
+        passwordPlaceholder: "Contraseña",
+        submit: "Enviar",
+        signUp: "Regístrate",
+        //nav bar text
+        home: "Casa",
+        facilityUpload: "Subida de instalaciones",
     }
 });
 
-export default strings;
+export default localizationStrings;
