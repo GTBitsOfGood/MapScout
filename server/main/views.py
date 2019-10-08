bc8daaec016b2e36fe1db6caa024e9fb1bb0a369
Adib Khan <adib.khan@gmail.com>
2019-10-08 18:33:22 -0400

    Deleted server dir

--- server/main/views.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, url_for, request, jsonify, make_response
-from . import main
-
-@main.route('/api/test')
-def index():
-  '''Sample API route for data'''
-  print('here')
-  return jsonify([{'title': 'A'}, {'title': 'C'}])
