ec92a8a2024cac49e4d3cfc890984b57ff66e762
Jeff Zhan <jeffzhan2008@gmail.com>
2019-10-15 12:50:34 -0400

    another quick update

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -3,6 +3,7 @@ import NavBar from './NavBar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'; 
 
 class Index extends Component {
 
