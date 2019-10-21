77015b08a96dd364f3c83ce9d0167c0d86ed02f3
Rahul Tholakapalli <rahultholakapalli@gmail.com>
2019-10-21 04:41:59 -0400

    Enabled adding provider data

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -22,9 +22,51 @@ class Dashboard extends Component {
             this.setState({providers: this.state.providers.filter(entry => entry['provider'] != document)});
         };
 
+        this.addEntry = async () => {
+            await this.props.firebase
+                .firestore()
+                .collection("providers").doc(this.state.provider).set({
+                    address: this.state.address,
+                    ages: this.state.ages,
+                    buildingNum: this.state.buildingNum,
+                    childcare: this.state.childcare,
+                    epic: this.state.epic,
+                    hours: this.state.hours,
+                    insurance: this.state.insurance,
+                    languages: this.state.languages,
+                    notes: this.state.notes,
+                    phoneNum: this.state.phoneNum,
+                    serviceType: this.state.serviceType,
+                    specializations: this.state.specializations,
+                    therapyTypes: this.state.therapyTypes,
+                    website: this.state.website,
+                    weekendHours: this.state.weekendHours
+                }).then(function() {
+                alert("Successfully added provider!");
+            });
+            //this.setState({providers: [...this.state.providers, this.state.provider]});
+            this.getFirebase()
+        };
+
         this.state = {
             files: [],
             providers: [],
+            provider: "",
+            address: "",
+            ages: "",
+            buildingNum: "",
+            childcare: "",
+            epic: "",
+            hours: "",
+            insurance: "",
+            languages: "",
+            notes: "",
+            phoneNum: "",
+            serviceType: "",
+            specializations: "",
+            therapyTypes: "",
+            website: "",
+            weekendHours: ""
         };
     }
 
@@ -53,6 +95,21 @@ class Dashboard extends Component {
 
     componentDidMount(){
         this.getFirebase();
+    }
+
+    handleChange = (event) => {
+        const name = event.target.name;
+        const val = event.target.value;
+        this.setState({
+            [name]:val
+        })
+        // console.log(name + " " + val);
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.addEntry();
+        document.getElementById("add-provider-form").reset();
     }
 
     render() {
@@ -89,7 +146,25 @@ class Dashboard extends Component {
                                 <input {...getInputProps()} />
                                 <p>Drag and drop some files here, or click to select files</p>
                             </div>
-
+                            <form id="add-provider-form" onSubmit={this.handleSubmit}>
+                                <input type="text" name="provider" placeholder="Provider" onChange={this.handleChange}/>
+                                <input type="text" name="address" placeholder="Address" onChange={this.handleChange}/>
+                                <input type="text" name="ages" placeholder="Ages" onChange={this.handleChange}/>
+                                <input type="text" name="buildingNum" placeholder="Building Number" onChange={this.handleChange}/>
+                                <input type="text" name="childcare" placeholder="Childcare" onChange={this.handleChange}/>
+                                <input type="text" name="epic" placeholder="Epic" onChange={this.handleChange}/>
+                                <input type="text" name="hours" placeholder="Hours" onChange={this.handleChange}/>
+                                <input type="text" name="insurance" placeholder="Insurance" onChange={this.handleChange}/>
+                                <input type="text" name="languages" placeholder="Languages" onChange={this.handleChange}/>
+                                <input type="text" name="notes" placeholder="Notes" onChange={this.handleChange}/>
+                                <input type="text" name="phoneNum" placeholder="Phone Number" onChange={this.handleChange}/>
+                                <input type="text" name="serviceType" placeholder="Service Type" onChange={this.handleChange}/>
+                                <input type="text" name="specializations" placeholder="Specializations" onChange={this.handleChange}/>
+                                <input type="text" name="therapyTypes" placeholder="Therapy Types" onChange={this.handleChange}/>
+                                <input type="text" name="website" placeholder="Website" onChange={this.handleChange}/>
+                                <input type="text" name="weekendHours" placeholder="Weekend Hours" onChange={this.handleChange}/>
+                                <input type="submit" value="Add Provider" />
+                            </form>
                             <aside>
                                 <h4>Files</h4>
                                 <ul>{files}</ul>
@@ -98,6 +173,9 @@ class Dashboard extends Component {
 
                     )}
                 </Dropzone>
+                <div>
+                
+                </div>
                 <section className="container">
                     <ReactTable
                         data={data}
