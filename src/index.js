19552fe9cdaee15b5588edfd8cf6037f80de052a
Arthelon <hsing.daniel@gmail.com>
2020-06-07 20:05:20 +0800

    feat(build): replace old webpack setup with 'react-scripts'
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
-import 'babel-polyfill';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
-import { store, history } from './store';
-import { routes } from './routes';
-import './assets/styles/style';
+import "babel-polyfill";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import { store, history } from "./store";
+import { routes } from "./routes";
+import "./assets/styles/style";
 
 const getRoutes = async () => {
-  const strRoutes = await store.firestore.collection("teams")
-    .get()
-    .then((doc) => {
-      const arr = [];
-      doc.forEach((team) => {
-        arr.push("/" + team.id);
-      });
-      return arr;
-    })
-  
-  // render the main component
-  ReactDOM.render(
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        {routes(strRoutes)}
-      </ConnectedRouter>
-    </Provider>,
-    document.getElementById('app'),
-  );
-}
+    const strRoutes = await store.firestore
+        .collection("teams")
+        .get()
+        .then((doc) => {
+            const arr = [];
+            doc.forEach((team) => {
+                arr.push("/" + team.id);
+            });
+            return arr;
+        });
+
+    // render the main component
+    ReactDOM.render(
+        <Provider store={store}>
+            <ConnectedRouter history={history}>
+                {routes(strRoutes)}
+            </ConnectedRouter>
+        </Provider>,
+        document.getElementById("app")
+    );
+};
 
 getRoutes();
