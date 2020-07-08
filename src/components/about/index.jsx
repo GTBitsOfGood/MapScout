5c4eec41b833410e0d9cb6948093dbab6d4b6259
Adib Khan <adib.khan@gmail.com>
2020-07-08 03:40:54 -0400

    Initial About Us Page

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import GOOGLE_API_KEY from '../../config/keys';
 import mapConfig from './mapConfig';
+import markersConfig from './markersConfig';
+import TeamMarker from './TeamMarker';
+
+const lat = 33.7756;
+const lng = -84.3963;
+const coords = { lat, lng };
 
 function AboutUs() {
   const mapOptions = {
@@ -14,10 +20,21 @@ function AboutUs() {
         bootstrapURLKeys={{
           key: GOOGLE_API_KEY,
         }}
-        zoom={17}
-        defaultCenter={{ lat: 33.7756, lng: -84.3963 }}
+        zoom={17.8}
+        defaultCenter={coords}
         options={mapOptions}
-      />
+      >
+        {
+          markersConfig.map((member) => (
+            <TeamMarker
+              key={member.name}
+              lat={member.lat}
+              lng={member.lng}
+              member={member}
+            />
+          ))
+        }
+      </GoogleMapReact>
     </div>
   );
 }
