14e85c0b1248bd8e52eda72889d85ea02066ca7f
Luke Kim <lukekim@Lukes-MacBook-Pro.local>
2020-07-15 22:43:42 -0400

    chat feature basic

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+import 'firebase/database'
+import {chatRef} from '../../store';
+
+
 
 function Chat() {
+  const [message, setMessage] = useState("");
+  const addToDo = async (newToDo) => {
+    chatRef.push().set(newToDo);
+  };
+  
+  const inputChange = e => {
+    setMessage(e.target.value);
+  };
+  
+  const formSubmit = () => {
+    var currentdate = new Date(); 
+    var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    addToDo({
+      message: message,
+      timestamp: datetime,
+      uid:"luke",
+      username:"luke"
+    });
+    setMessage("");
+  };
+
   return (
-    <h1>YEET</h1>
+       <div>
+        <form> 
+          <input type="text" onChange={e => inputChange(e)} onSubmit={formSubmit}/>
+          <input type="submit" value="submit" onClick={formSubmit}/>
+        </form>
+       </div>
   );
 }
 
