6303e65fabbe653edb0f65eb13591e16a0713f0c
Adib Khan <adib.khan@gmail.com>
2020-07-29 21:11:06 -0400

    Forgot to git add

---
+++ src/routes/pathnames.js
@@ -0,0 +1,9 @@
+export const homeRoute = '/';
+export const aboutRoute = '/about';
+export const authRoute = '/auth';
+export const pwdRoute = '/forgot';
+// Private routes
+export const providerRoute = '/provider';
+export const formRoute = '/provider/add';
+export const templateRoute = '/provider/template';
+export const chatRoute = '/provider/feeback';
