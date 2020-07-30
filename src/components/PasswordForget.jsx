3842fe4fa71323743c19e38cb9870d7ded5ca1f1
Adib Khan <adib.khan@gmail.com>
2020-07-29 20:08:42 -0400

    Password Forget hooked

--- src/components/PasswordForget.jsx
+++ src/components/PasswordForget.jsx
@@ -1,126 +1,125 @@
-import React, { Component } from "react";
-import { withFirebase } from "react-redux-firebase";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Alert from "react-bootstrap/Alert";
+import React, { useState } from 'react';
+import { withFirebase } from 'react-redux-firebase';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 
-import localizationStrings from "../utils/Localization";
-import { authRoute } from "./ProviderRoutes";
+import localizationStrings from '../utils/Localization';
+import { authRoute } from './ProviderRoutes';
 
-const classNames = require("classnames");
+const classNames = require('classnames');
 
 const INITIAL_STATE = {
-    email: "",
-    error: null,
-    animate: false,
-    isLoading: false,
-    submitted: false
+  email: '',
+  error: null,
+  animate: false,
+  isLoading: false,
+  submitted: false,
 };
 
-class PasswordForgetFormBase extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { ...INITIAL_STATE };
-        this.onSubmit = this.onSubmit.bind(this);
-        this.onChange = this.onChange.bind(this);
-    }
+function PasswordForgetFormBase({ firebase }) {
+  const [email, setEmail] = useState(INITIAL_STATE.email);
+  const [error, setError] = useState(INITIAL_STATE.error);
+  const [animate, setAnimate] = useState(INITIAL_STATE.animate);
+  const [isLoading, setIsLoading] = useState(INITIAL_STATE.isLoading);
+  const [submitted, setSubmitted] = useState(INITIAL_STATE.submitted);
 
-    onSubmit(event) {
-        const { email } = this.state;
-        this.setState({ isLoading: true });
-        event.preventDefault();
-        this.props.firebase.auth().useDeviceLanguage();
-        this.props.firebase
-            .auth()
-            .sendPasswordResetEmail(email)
-            .then(() => {
-                this.setState({ ...INITIAL_STATE, submitted: true });
-            })
-            .catch(error => {
-                this.setState({ error: error.message, isLoading: false });
-            });
-    }
+  function onSubmit(event) {
+    event.preventDefault();
+    setIsLoading(true);
+    firebase.auth().useDeviceLanguage();
+    firebase
+      .auth()
+      .sendPasswordResetEmail(email)
+      .then(() => {
+        setEmail(INITIAL_STATE.email);
+        setError(INITIAL_STATE.error);
+        setAnimate(INITIAL_STATE.animate);
+        setAnimate(INITIAL_STATE.isLoading);
+        setSubmitted(true);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
+  }
 
-    onChange(event) {
-        this.setState({ [event.target.type]: event.target.value });
-    }
+  function onChange(event) {
+    setEmail(event.target.value);
+  }
 
-    render() {
-        const { email, error, animate, isLoading, submitted } = this.state;
-        const {
-            resetPassword,
-            submit,
-            emailLabel,
-            emailPlaceholder,
-            login,
-            sent,
-        } = localizationStrings;
+  const {
+    resetPassword,
+    submit,
+    emailLabel,
+    emailPlaceholder,
+    login,
+    sent,
+  } = localizationStrings;
 
-        const isInvalid = email === "";
-        return (
-            <Container fluid>
-                <div
-                    id="auth-root"
-                    className={classNames("box", { translate: animate })}
-                >
-                    {!submitted && (
-                        <Form onSubmit={this.onSubmit}>
-                            <div className="mb-4">
-                                <h2>{resetPassword}</h2>
-                            </div>
-                            <Form.Group controlId="formEmail">
-                                <Form.Label>{emailLabel}</Form.Label>
-                                <Form.Control
-                                    type="email"
-                                    placeholder={emailPlaceholder}
-                                    onChange={this.onChange}
-                                />
-                            </Form.Group>
-                            <br />
-                            {error ? (
-                                <Alert
-                                    variant="danger"
-                                    onClose={() =>
-                                        this.setState({ error: null })
-                                    }
-                                    dismissible
-                                >
-                                    <small>{error}</small>
-                                </Alert>
-                            ) : null}
-                            <Button
-                                variant="primary"
-                                onClick={!isLoading ? this.onSubmit : null}
-                                disabled={isLoading || isInvalid}
-                                block
-                            >
-                                {isLoading && <div className="loader" />}{" "}
-                                {submit}
-                            </Button>
-                        </Form>
-                    )}
-                    {submitted && (
-                        <div>
-                        <div className="mb-4">
-                            <h2>{resetPassword}</h2>
-                        </div>
-                            <div className="mb-4">{sent}</div>
-                            <Button
-                                variant="primary"
-                                href={authRoute}
-                                block
-                            >
-                                {login}
-                            </Button>
-                        </div>
-                    )}
-                </div>
+  const isInvalid = email === '';
+  return (
+    <Container fluid>
+      <div
+        id="auth-root"
+        className={classNames('box', { translate: animate })}
+      >
+        {!submitted && (
+        <Form onSubmit={onSubmit}>
+          <div className="mb-4">
+            <h2>{resetPassword}</h2>
+          </div>
+          <Form.Group controlId="formEmail">
+            <Form.Label>{emailLabel}</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder={emailPlaceholder}
+              onChange={onChange}
+            />
+          </Form.Group>
+          <br />
+          {error ? (
+            <Alert
+              variant="danger"
+              onClose={() => setError(INITIAL_STATE.error)}
+              dismissible
+            >
+              <small>{error}</small>
+            </Alert>
+          ) : null}
+          <Button
+            variant="primary"
+            onClick={!isLoading ? onSubmit : null}
+            disabled={isLoading || isInvalid}
+            block
+          >
+            {isLoading && <div className="loader" />}
+            {' '}
+            {submit}
+          </Button>
+        </Form>
+        )}
+        {submitted && (
+        <div>
+          <div className="mb-4">
+            <h2>{resetPassword}</h2>
+          </div>
+          <div className="mb-4">{sent}</div>
+          <Button
+            variant="primary"
+            href={authRoute}
+            block
+          >
+            {login}
+          </Button>
+        </div>
+        )}
+      </div>
 
-                <div className="splash" />
-            </Container>
-        );
-    }
+      <div className="splash" />
+    </Container>
+  );
 }
 
 const PasswordForgetForm = withFirebase(PasswordForgetFormBase);
