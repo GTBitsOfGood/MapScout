da81758780699c008507d664119054e4de530c4f
Adib Khan <adib.khan@gmail.com>
2020-09-04 15:26:42 -0400

    feat: add firestore types

---
+++ src/types/firestore.ts
@@ -0,0 +1,59 @@
+export type TeamDocProps = {
+  label: string,
+  latitude: number,
+  longitude: number,
+  logoUrl?: string,
+  name: string,
+  primaryColor: string,
+  secondaryColor: string,
+  zoom?: number
+}
+
+export type UserDocProps = {
+  UID: string,
+  email: string,
+  team: string
+}
+
+export type CategoryOptions = {
+  label: string,
+  value: string,
+}
+
+export type CategoryDocProps = {
+  active: boolean,
+  id: string,
+  name: string,
+  options?: CategoryOptions[],
+  priority: number,
+  select_type: number,
+  team: string,
+}
+
+export type ProviderHours = {
+  Friday: number[],
+  Monday: number[],
+  Saturday: number[],
+  Sunday: number[],
+  Thursday: number[],  
+  Tuesday: number[],
+  Wednesday: number[],
+}
+
+export type ProviderProps = {
+  address: string,
+  buildingNum?: string,
+  description?: string,
+  facilityName: string,
+  hours?: ProviderHours,
+  id: string,
+  image?: string,
+  imageURL?: string,
+  latitude: number,
+  longitude: number,
+  phoneNum: string,
+  website?: string,
+  team: string
+} & {
+  [prop: string]: CategoryDocProps;
+};
