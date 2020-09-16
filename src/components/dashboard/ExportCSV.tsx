689655e674782879daa06a563627f1029c6ffd13
akrishnan93 <anand.krrishnan@gmail.com>
2020-09-16 13:28:33 -0400

    Fixed formatting issues on the downloaded CSV

--- src/components/dashboard/ExportCSV.tsx
+++ src/components/dashboard/ExportCSV.tsx
@@ -7,59 +7,34 @@ const ExportCSV = (props) => {
 
   function arrayToJson() {
     //Make a providers temp array to edit
-    let providersCopy : {team: string}[] = Array.from(props.providers);
+    let providersCopy : {team: string, hours: string}[] = Array.from(props.providers);
 
     //iterates through providers array and converts each provider to a JSON object
-    for (let index = 0; index < providersCopy.length; index++) {
-      //deletes the team property from each array
-      delete providersCopy[index].team;
+    for (let index = 0; index < props.providers.length; index++) {
+      providersCopy[index].hours = JSON.stringify(providersCopy[index].hours);  //stringify the hour property
+      delete providersCopy[index].team;   //deletes the team property from each provider
     }
-    return JSON.stringify(providersCopy);
+
+    return providersCopy;
   }
 
   function handleClick() {
-    const jsonData =
-    `[{
-        "Column 1": "1",
-        "Column 2": "2",
-        "Column 3": "3",
-        "Column 4": "4"
-      },
-      {
-        "Column 1": "5",
-        "Column 2": "6",
-        "Column 3": "7",
-        "Column 4": "8"
-      },
-      {
-        "Column 1": "9",
-        "Column 2": "10",
-        "Column 3": "11",
-        "Column 4": "12"
-      },
-      {
-        "Column 1": 14,
-        "Column 2": 15,
-        "Column 3": 16,
-        "Column 4": 17
-      }]`
+    const columns = ["address", "buildingNum", "description", "facilityName", "hours", "id",
+                      "image", "imageURL", "latitude", "longitude", "phoneNum", "website"];
 
     const config = {
-      quotes: false, //or array of booleans
+      quotes: true, //or array of booleans
       quoteChar: '"',
       escapeChar: '"',
       delimiter: ",",
       header: true,
-      newline: "{",
-      skipEmptyLines: false, //or 'greedy',
-      columns: null //or array of strings
+      newline: "\n",
+      skipEmptyLines: 'false', //or 'greedy',
+      columns: columns //or array of strings
     }
 
     const results = jsonToCSV(arrayToJson(), config);
-    console.log(results);
-
     var dataString = results.split(/\r?\n/);
-    //dataString = dataString.slice(1);
     var dataArray = [];
     for(var i =0; i < dataString.length; i++){
       var subrray = [dataString[i]];
