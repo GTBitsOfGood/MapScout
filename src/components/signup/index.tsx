c8ef67b4d4508472ff95e790abed3e7d8b7e696c
sjacob2019 <shaunjacob01@gmail.com>
2020-09-22 17:30:01 -0400

    Connecting signup page with firebase

--- src/components/signup/index.tsx
+++ src/components/signup/index.tsx
@@ -4,69 +4,79 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 
-// import { withFirebase } from 'react-redux-firebase';
+import { withFirebase } from 'react-redux-firebase';
 
 import Container from 'react-bootstrap/Container';
 import Blur from '@animate/blur';
 import localizationStrings from '../../utils/Localization';
 import { providerRoute, pwdRoute } from '../../routes/pathnames';
+import { FaSlack } from 'react-icons/fa';
 
 const classNames = require('classnames');
 
-function SignUp({ firebase, history }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
-  const [animate, setAnimate] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [passwordConfirm, setPasswordConfirm] = useState('');
-  const [passwordsMatch, setPasswordsMatch] = useState(false);
+  function SignUp({ firebase, history }) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const [animate, setAnimate] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [passwordConfirm, setPasswordConfirm] = useState('');
+    const [passwordsMatch, setPasswordsMatch] = useState(false);
 
-function handleChange(e) {
-  const { value, type } = e.target;
-  if (type === "email") {
-    setEmail(value);
-    console.log('setting email to: ' + email)
-  } else {
-    setPassword(value);
-    console.log('setting password to: ' + value);
-    if (value === passwordConfirm) {
+  function handleChange(e) {
+    const { value, type } = e.target;
+    if (type === "email") {
+      setEmail(value);
+      console.log('setting email to: ' + email)
+    } else {
+      setPassword(value);
+      console.log('setting password to: ' + value);
+      if (value === passwordConfirm) {
+        setPasswordsMatch(true);
+      } else {
+        setPasswordsMatch(false);
+      }
+    } 
+  }
+
+  function handleConfirmPasswordChange(e) {
+    const { value } = e.target;
+    setPasswordConfirm(value);
+    console.log('setting confirm password to: ' + value);
+    if (value === password) {
       setPasswordsMatch(true);
     } else {
       setPasswordsMatch(false);
     }
-  } 
-}
-
-function handleConfirmPasswordChange(e) {
-  const { value } = e.target;
-  setPasswordConfirm(value);
-  console.log('setting confirm password to: ' + value);
-  if (value === password) {
-    setPasswordsMatch(true);
-  } else {
-    setPasswordsMatch(false);
   }
-}
 
-//   async function handleSubmit(e) {
-//     e.preventDefault();
-//     setIsLoading(true);
-//     try {
-//       const response = await firebase
-//         .auth()
-//         .signInWithEmailAndPassword(email, password);
-//       setAnimate(true);
-//       await setTimeout(() => {
-//         setIsLoading(false);
-//         history.push(providerRoute);
-//       }, 400);
-//     } catch (err) {
-//       // TODO: Add translations
-//       setError(err.message);
-//       setIsLoading(false);
-//     }
-//   }
+  function createUserWithEmailAndPassword({ email, password }) {
+    firebase.createUser (
+      { email, password },
+    )
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    createUserWithEmailAndPassword({ 
+      email,
+      password,
+    });
+    setIsLoading(true);
+    try {
+      const response = await firebase
+        .auth()
+      setAnimate(true);
+      await setTimeout(() => {
+        setIsLoading(false);
+        history.push(providerRoute);
+      }, 400);
+    } catch (err) {
+      // TODO: Add translations
+      setError(err.message);
+      setIsLoading(false);
+    }
+  }
 
   const {
     emailLabel, emailPlaceholder, passwordLabel, passwordPlaceholder, confirmPasswordLabel, createAccount, create
@@ -77,12 +87,12 @@ function handleConfirmPasswordChange(e) {
     <Container fluid>
       <div id="auth-root" className={classNames('box', { translate: animate })}>
         <Form
-          // onSubmit={handleSubmit}
-          // onKeyPress={(e) => {
-          //   if (e.key === 'Enter') {
-          //     handleSubmit(e);
-          //   }
-          // }}
+          onSubmit={handleSubmit}
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              handleSubmit(e);
+            }
+          }}
         >
           <div className="mb-4">
             <h2>{createAccount}</h2>
@@ -111,7 +121,7 @@ function handleConfirmPasswordChange(e) {
           }
           <Button
             variant="primary"
-            // onClick={handleSubmit}
+            onClick={handleSubmit}
             disabled={isLoading || !passwordsMatch || !password || !email}
             block
           >
@@ -134,4 +144,4 @@ function handleConfirmPasswordChange(e) {
   );
 }
 
-export default SignUp;
+export default withFirebase(SignUp);
