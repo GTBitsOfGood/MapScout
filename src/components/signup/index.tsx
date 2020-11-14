86e4fd9d0cdb63c7e7b604f354ea51e3602b10fb
sjacob2019 <shaunjacob01@gmail.com>
2020-11-14 12:51:44 -0500

    Adding images for new pages

--- src/components/signup/index.tsx
+++ src/components/signup/index.tsx
@@ -95,65 +95,69 @@ const classNames = require('classnames');
     emailLabel, emailPlaceholder, passwordLabel, passwordPlaceholder, confirmPasswordLabel, createAccount, create
   } = localizationStrings;
 
-  console.log(email)
   return (
-    <Container fluid>
-      <div id="auth-root" className={classNames('box', { translate: animate })}>
-        <Form
-          onSubmit={handleSubmit}
-          onKeyPress={(e) => {
-            if (e.key === 'Enter') {
-              handleSubmit(e);
-            }
-          }}
-        >
-          <div className="mb-4">
-            <h2>{createAccount}</h2>
-          </div>
-          <Form.Group controlId="formEmail">
-            <Form.Label>{emailLabel}</Form.Label>
-            <Form.Control type="email" placeholder={emailPlaceholder} onChange={handleChange} />
-          </Form.Group>
-          <Form.Group controlId="formPassword">
-            <Form.Label>{passwordLabel}</Form.Label>
-            <Form.Control type="password" placeholder={passwordPlaceholder} onChange={handleChange} />
-          </Form.Group>
-          <Form.Group controlId="formPassword">
-            <Form.Label>{confirmPasswordLabel}</Form.Label>
-            <Form.Control type="password" placeholder={passwordPlaceholder} onChange={handleConfirmPasswordChange} />
-          </Form.Group>
-          <br />
-          {
-            error
-            ? (
-              <Alert variant="danger" onClose={() => setError(null)} dismissible>
-                <small>{error}</small>
-              </Alert>
-            )
-            : null
-          }
-          <Button
-            variant="primary"
-            onClick={handleSubmit}
-            disabled={isLoading}
-            block
-          >
-            {isLoading && <div className="loader" />}
-            {' '}
-            {create}
-          </Button>
-        </Form>
+    <div>
+      <div>
+        
       </div>
-      {
-        animate
-        ? (
-          <Blur>
-            <div className="splash fade-out" />
-          </Blur>
-        )
-        : <div className="splash" />
-      }
-    </Container>
+      <div id="signup-root"> 
+        <div id="auth-root" className={classNames('box', { translate: animate })}>
+          <Form
+            onSubmit={handleSubmit}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                handleSubmit(e);
+              }
+            }}
+          >
+            <div className="mb-4">
+              <h2>{createAccount}</h2>
+            </div>
+            <Form.Group controlId="formEmail">
+              <Form.Label>{emailLabel}</Form.Label>
+              <Form.Control type="email" placeholder={emailPlaceholder} onChange={handleChange} />
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label>{passwordLabel}</Form.Label>
+              <Form.Control type="password" placeholder={passwordPlaceholder} onChange={handleChange} />
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label>{confirmPasswordLabel}</Form.Label>
+              <Form.Control type="password" placeholder={passwordPlaceholder} onChange={handleConfirmPasswordChange} />
+            </Form.Group>
+            <br />
+            {
+              error
+              ? (
+                <Alert variant="danger" onClose={() => setError(null)} dismissible>
+                  <small>{error}</small>
+                </Alert>
+              )
+              : null
+            }
+            <Button
+              variant="primary"
+              onClick={handleSubmit}
+              disabled={isLoading}
+              block
+            >
+              {isLoading && <div className="loader" />}
+              {' '}
+              {create}
+            </Button>
+          </Form>
+        </div>
+        {/* {
+          animate
+          ? (
+            <Blur>
+              <div className="splash fade-out" />
+            </Blur>
+          )
+          : <div className="splash" />
+        } */}
+      </div>
+    </div>
   );
 }
 
