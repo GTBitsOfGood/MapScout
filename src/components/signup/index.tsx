31fed2f6287faa67017ce583991f429d28632751
luke9kim8 <luke9kim8@gmail.com>
2020-11-16 05:06:28 +0900

    modify signup page with logos

--- src/components/signup/index.tsx
+++ src/components/signup/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 
@@ -97,33 +99,48 @@ const classNames = require('classnames');
 
   return (
     <div>
-      <div>
-        
-      </div>
+      <div className="navBar-signup">
+          <Button className="navBar-signup-button"><img src={require("../../assets/img/horizontal_lockup.png")}/> </Button>
+          <Button className="navBar-signup-button" style={{color: "#0A1D7C"}}> About </Button>
+          <Button className="navBar-signup-button" style={{color: "#0A1D7C"}}> Contact Us </Button>
+          <Button className="navBar-signup-button"><img src={require("../../assets/img/user 1.png")}/> </Button>
+        </div>
       <div id="signup-root"> 
-        <div id="auth-root" className={classNames('box', { translate: animate })}>
-          <Form
+      
+        <div id="auth-root">
+        
+          <Form 
             onSubmit={handleSubmit}
             onKeyPress={(e) => {
               if (e.key === 'Enter') {
                 handleSubmit(e);
               }
             }}
+            className="signup-form"
           >
             <div className="mb-4">
-              <h2>{createAccount}</h2>
+              <h2>Set-up your account</h2>
             </div>
-            <Form.Group controlId="formEmail">
-              <Form.Label>{emailLabel}</Form.Label>
-              <Form.Control type="email" placeholder={emailPlaceholder} onChange={handleChange} />
+            <div className="mapscout-girl-logo-div">
+              <img className="mapscout-girl-logo-img" src={require('../../assets/img/undraw_my_current_location_om7g 1.svg')}/>
+            </div>
+            <Form.Group as={Row} controlId="formEmail" className="signup-form-group">
+              <Form.Label column sm="2" className="signup-form-label">Email</Form.Label>
+              <Col sm="10">
+                <Form.Control className="signup-form-textfield" type="email" placeholder={emailPlaceholder} onChange={handleChange} />
+              </Col>
             </Form.Group>
-            <Form.Group controlId="formPassword">
-              <Form.Label>{passwordLabel}</Form.Label>
-              <Form.Control type="password" placeholder={passwordPlaceholder} onChange={handleChange} />
+            <Form.Group as={Row} controlId="formPassword" className="signup-form-group">
+              <Form.Label column sm="2">{passwordLabel}</Form.Label>
+              <Col sm="10">
+              <Form.Control className="signup-form-textfield" type="password" placeholder={passwordPlaceholder} onChange={handleChange} />
+              </Col>
             </Form.Group>
-            <Form.Group controlId="formPassword">
-              <Form.Label>{confirmPasswordLabel}</Form.Label>
-              <Form.Control type="password" placeholder={passwordPlaceholder} onChange={handleConfirmPasswordChange} />
+            <Form.Group as={Row} controlId="formPassword" className="signup-form-group">
+              <Form.Label column sm="2" >Verify</Form.Label>
+              <Col sm="10"> 
+                <Form.Control className="signup-form-textfield" type="password" placeholder={passwordPlaceholder} onChange={handleConfirmPasswordChange} />
+              </Col>
             </Form.Group>
             <br />
             {
@@ -140,13 +157,16 @@ const classNames = require('classnames');
               onClick={handleSubmit}
               disabled={isLoading}
               block
+              style={{backgroundColor:"#0A1D7C", width:"80%", height:"60px"}}
             >
               {isLoading && <div className="loader" />}
               {' '}
-              {create}
+              Submit
             </Button>
           </Form>
+         
         </div>
+        
         {/* {
           animate
           ? (
---
+++ src/components/signupdivider/index.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+
+const SignUpDivider = () => {
+    alert("hey")
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default SignUpDivider;
