9a4b9cfaea20ac5d7d2cb199dc3eed6d22282be5
Sethi, Aashna <asethi44@gatech.edu>
2021-11-09 15:00:49 -0500

    Removed .env dependency for firebase

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -1,11 +1,14 @@
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey:'AIzaSyDhA6ue9yEMupXLN7MyZPHkrp2bXs_KlSA',
+  authDomain:'gtbog-pacts.firebaseapp.com',
+  databaseURL:'https://gtbog-pacts.firebaseio.com',
+  projectId:'gtbog-pacts',
+  storageBucket:'gtbog-pacts.appspot.com',
+  messagingSenderId:973317690227,
+  appId:'1:973317690227:web:4c9e435640d534914b2b06',
+  googleKey:'AIzaSyAtD3k03RcZx7VI6ahZ75IcUDNIklSUMNg',
+  sentryKey:'https://79c9ce9a9592488d94e668d4e2c89bb2@o415387.ingest.sentry.io/5306392',
+  prismicKey:'MC5YMkVkUVJJQUFDY0FjU19f.Le-_vS3vv71q77-9BO-_ve-_ve-_ve-_vT3vv73vv70G77-9Ru-_ve-_vQTvv73vv71eX--_vXYKPe-_ve-_vRpN',
 };
 
 export default firebaseConfig;
