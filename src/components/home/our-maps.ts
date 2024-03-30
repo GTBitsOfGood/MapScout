e7b06e8dacbdc406ded1b2d6380ccd4cf1d67486
ShivaniVora <shivanivora797@icloud.com>
2024-03-30 12:27:06 -0400

    Squashed commit of the following:
    commit 0d6db9966ad64dc423fb781decdfe1a60220fbe6
    Merge: 7c3a7f5 be40b51
    Author: avayedawadi <45608655+avayedawadi@users.noreply.github.com>
    Date:   Sat Mar 30 12:23:28 2024 -0400
    
        Merge pull request #268 from GTBitsOfGood/cduffy/refactor-home-page
    
        final clean up
    
    commit be40b5145aebd3d664f9b1aa9c5b7aea33c443f1
    Author: cduffy8 <connorpduffy160182@gmail.com>
    Date:   Sat Mar 30 12:03:14 2024 -0400
    
        final clean up

--- src/components/home/our-maps.ts
+++ src/components/home/our-maps.ts
@@ -2,16 +2,11 @@ export const ourMaps = [
     {
       title: "The Philadelphia Alliance for Child Trauma Services (PACTS)",
       description: "PACTS is a network of child serving systems and organizations that provides evidence-based practices for traumatized youth and their families in the greater Philadelphia area.",
-      image: "TODO"
+      image: "./map2.png"
     },
     {
       title: "ClinWiki",
       description: "A 501(c)(3) focused on making clinical trials more transparent and approachable in order to drive participation and faster progress against serious diseases.",
-      image: "TODO"
-    },
-    {
-      title: "Georgia DBHDD",
-      description: "Organization description Organization description Organization description Organization description Organization description",
-      image: "TODO"
+      image: "./cliniwiki.png"
     }
 ];
