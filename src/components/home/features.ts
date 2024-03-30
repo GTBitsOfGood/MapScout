be40b5145aebd3d664f9b1aa9c5b7aea33c443f1
cduffy8 <connorpduffy160182@gmail.com>
2024-03-30 12:03:14 -0400

    final clean up

--- src/components/home/features.ts
+++ src/components/home/features.ts
@@ -4,17 +4,20 @@ export const features = [
       description:
         "Admins can smoothly manipulate anywhere from ten to ten thousand custom, presented data points",
       videoPath: "./createProvider.mov",
+      poster: "./mapInfo.png",
     },
     {
       title: "Create powerful search filters",
       description:
         "Our unique template builder enables visitors to intuitively search with fully controlled, customizable categories.",
       videoPath: "./mapWalkthrough.mov",
+      poster: "./map2.png",
     },
     {
       title: "Integrate Mapscout with your website",
       description:
         "Mapscout is both web and mobile friendly and can be effortlessly added to existing sites.",
       videoPath: "./templateBuilder.mov",
+      poster: "./map1.png",
     }
 ];
